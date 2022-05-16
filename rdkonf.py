9a2be276183382aab2d8b76cf0e09d9a671bd14b
Steven Shave <stevenshave@gmail.com>
2022-05-16 14:08:38 +0100

    Removed unneeded energy minimisation

--- rdkonf.py
+++ rdkonf.py
@@ -1,93 +1,121 @@
-#/ Copyright 2019 Steven Shave (stevenshave@gmail.com)
-#/ rdkonf - v1.04
-#/ Implementation of high quality RDKit conformer generator as described in:
-#/ Ebejer, Jean-Paul, Garrett M. Morris, and Charlotte M. Deane.
-#/ "Freely available conformer generation methods: how good are they?."
-#/ Journal of chemical information and modeling 52.5 (2012): 1146-1158.
-#/ Please reference this code/repository : https://github.com/stevenshave/rdkonf
-#/ in any publication.
-#//////////////////////////////
+#!/usr/bin/env python
 
+"""
+Implementation of high quality RDKit conformer generator as described in:
+Ebejer, Jean-Paul, Garrett M. Morris, and Charlotte M. Deane. 'Freely available
+conformer generation methods: how good are they?.' Journal of chemical
+information and modeling 52.5 (2012): 1146-1158.
+
+"""
+
+import argparse
+from pathlib import Path
 from rdkit import Chem
 from rdkit.Chem import AllChem
 import sys
 import time
-import argparse
 
 
-def shouldWeKeep(keep, mol, q):
-    for k in keep:
-        if(AllChem.GetBestRMS(mol, mol, refId=k[1], prbId=q)<=0.35):
-            #if(AllChem.GetBestRMS(mol, mol, refConfId=k[1], probeConfId=q)<=0.35):   #  Deprecated - previous argument calls to GetBestRMS
-            #print "No! - ", AllChem.GetBestRMS(mol, mol, refConfId=k[1], probeConfId=q)
+def should_we_keep_a_mol(keep_list, mol, q):
+    for k in keep_list:
+        if AllChem.GetBestRMS(mol, mol, refId=k[1], prbId=q) <= 0.35:
             return False
     return True
 
-def rdkonf(input_file, output_file, numtokeep, removehydrogens):
-    t00=time.time()
-    filetype=input_file[len(input_file)-3:]
-    suppl=None
-    if (filetype=="sdf"):
-        print("Reading SDF")
-        suppl=Chem.SDMolSupplier(input_file)
-    if (filetype=="smi" or filetype=="ism" or filetype=="can" or filetype=="txt"):
-        print("Reading SMILES")
-        suppl=Chem.SmilesMolSupplier(input_file,titleLine=False)
-    if(suppl==None):
-        print("Cannot operate on file of type", filetype)
-        sys.exit()
-    writer=Chem.SDWriter(output_file)
-    counter=0
-    for it in range(0,len(suppl)):
-        counter+=1
+
+def smiles_to_3dmol(smiles: str, title: str = ""):
+
+    splitsmiles = smiles.split()
+    if len(splitsmiles) > 1:
+        smiles = splitsmiles[0]
+        title = "".join(splitsmiles[1:])
+    mol = Chem.MolFromSmiles(smiles)
+    if mol:
+        e = []
+        keep = []
+        n = 300
+        mol = Chem.AddHs(mol)
+        mol = Chem.RemoveHs(mol)
+        nrot = Chem.rdMolDescriptors.CalcNumRotatableBonds(mol)
+        if nrot <= 12:
+            n = 200
+            if nrot <= 7:
+                n = 50
+        confIds = AllChem.EmbedMultipleConfs(mol, n)
+        for confId in confIds:
+            ff = AllChem.UFFGetMoleculeForceField(mol, confId=confId)
+            AllChem.UFFOptimizeMolecule(mol, confId=confId)
+            e.append(ff.CalcEnergy())
+            d = sorted(zip(e, confIds))
+        for conf in d:
+            if len(keep) < 1:
+                if should_we_keep_a_mol(keep, mol, conf[1]):
+                    keep.append(conf)
+        if len(keep) == 1:
+            mol.SetProp("_Name", title)
+            return mol
+    return None
+
+
+def rdkonf_on_sdf_file(input_file: Path):
+    output_file = f"{input_file}.sdf"
+    numtokeep = 1
+    removehydrogens = 1
+    t00 = time.time()
+    writer = Chem.SDWriter(output_file)
+    mol_counter = 0
+    smiles_f = open(str(input_file))
+    line = smiles_f.readline().strip()
+    while line is not None and len(line) > 3:
         try:
-            mol=suppl[it]
-            t0=time.time()
+            smiles, ids = line.split()
+            mol = Chem.MolFromSmiles(smiles)
+            mol.SetProp("_Name", ids)
+            mol_counter += 1
+            t0 = time.time()
             if mol:
-                name=str(mol.GetProp("_Name"))
-                e=[]
-                keep=[]
-                n=300
-                mol=Chem.AddHs(mol)
-                if removehydrogens==1:
-                    mol=Chem.RemoveHs(mol)
-                
-                nrot=Chem.rdMolDescriptors.CalcNumRotatableBonds(mol)
-                if(nrot<=12):
-                    n=200
-                    if(nrot<=7):
-                        n=50
-                print("Mol",counter, "of", len(suppl),"title =",str(mol.GetProp("_Name")), " Nrot = ", nrot, ", generating", numtokeep, "low E confs...")
-                confIds=AllChem.EmbedMultipleConfs(mol, n)
+                name = mol.GetProp("_Name")
+                e = []
+                keep = []
+                n = 300
+                mol = Chem.AddHs(mol)
+                if removehydrogens == 1:
+                    mol = Chem.RemoveHs(mol)
+
+                nrot = Chem.rdMolDescriptors.CalcNumRotatableBonds(mol)
+                if nrot <= 12:
+                    n = 200
+                    if nrot <= 7:
+                        n = 50
+                print(
+                    f"Mol {mol_counter}, title = {mol.GetProp('_Name')}, Nrot = {nrot}, generating {numtokeep} low E confs..."
+                )
+                confIds = AllChem.EmbedMultipleConfs(mol, n)
                 for confId in confIds:
-                    ff=AllChem.UFFGetMoleculeForceField(mol, confId=confId)
+                    ff = AllChem.UFFGetMoleculeForceField(mol, confId=confId)
                     AllChem.UFFOptimizeMolecule(mol, confId=confId)
-                    ff.Minimize()
                     e.append(ff.CalcEnergy())
-                    d=sorted(zip(e, confIds))
+                    d = sorted(zip(e, confIds))
                 for conf in d:
-                    if(len(keep)<numtokeep):
-                        if(shouldWeKeep(keep,mol, conf[1])):
+                    if len(keep) < numtokeep:
+                        if should_we_keep_a_mol(keep, mol, conf[1]):
                             keep.append(conf)
-                counter=1
+                counter = 1
                 for molout in keep:
-                    mol.SetProp("_Name",name+"_"+str(counter))
+                    mol.SetProp("_Name", name)
                     writer.write(mol, confId=molout[1])
-                    counter=counter+1
-                print("took", time.time()-t0, "seconds.")
+                    counter = counter + 1
+                print("took", time.time() - t0, "seconds.")
         except:
-            print("Error reading molecule #"+ str(counter))
+            print(f"Error making a molecule from:\n {line}")
             print(sys.exc_info())
-    writer.flush()
+        line = smiles_f.readline().strip()
     writer.close()
-    print("\nGenerated", numtokeep, "conformers for each of", len(suppl), "in", time.time()-t00, "seconds")
+    print(f"\nGenerated {numtokeep} in {time.time() - t00} seconds")
+
 
 if __name__ == "__main__":
-    parser=argparse.ArgumentParser(description="rdkonf conformer generation")
-    parser.add_argument('inFile', help="In file (SDF, or SMI)")
-    parser.add_argument('outFile', help="Out file (SDF)")
-    parser.add_argument('numConformers', help="The number of conformers per molecule to make")
-    parser.add_argument('removeHydrogens', help="Should molecules be written using implicit hydrogens (optional, default = 1)", nargs="?", default=1)
-
-    args=parser.parse_args()
-    rdkonf(args.inFile, args.outFile, int(args.numConformers), int(args.removeHydrogens))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("smiles_file", help="Smiles file")
+    args = parser.parse_args()
+    rdkonf_on_sdf_file(Path(args.smiles_file))
